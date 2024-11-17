a818c5dacdb666e48ac9544325312eaa98e95134
Taewoong-H <taeung2008@gmail.com>
2024-11-17 23:53:38 +0900

    chore: github page 배포

--- next.config.js
+++ next.config.js
@@ -5,22 +5,9 @@ const nextConfig = {
 
     return config
   },
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      },
-    ]
-  },
-  // output: 'standalone' 추가
-  output: 'standalone',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://4ghosw.github.io/EcoMap_Web2/' : '',
+  trailingSlash: true,
+  output: 'export',
 }
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,34 @@
 
 import tmapAPI from '@/api/tmapAPI'
 import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
+import { useSearchParams } from 'next/navigation'
+import { Suspense, useEffect, useState } from 'react'
 
 const NaverMap = dynamic(() => import('@/components/map/NaverMap'), {
   ssr: false,
   loading: () => <div className="h-screen w-screen bg-gray-200" />,
 })
 
-export default function Home({ searchParams }: { searchParams: { startX: string; startY: string; endX: string; endY: string } }) {
+export default function Home() {
+  const searchParams = useSearchParams()
+  const [params, setParams] = useState({
+    startX: '',
+    startY: '',
+    endX: '',
+    endY: '',
+  })
+
+  useEffect(() => {
+    setParams({
+      startX: searchParams.get('startX') || '',
+      startY: searchParams.get('startY') || '',
+      endX: searchParams.get('endX') || '',
+      endY: searchParams.get('endY') || '',
+    })
+  }, [searchParams])
   return (
     <Suspense fallback={<div className="h-screen w-screen bg-gray-200" />}>
-      <NaverMap {...searchParams} />
+      <NaverMap {...params} />
     </Suspense>
   )
 }
